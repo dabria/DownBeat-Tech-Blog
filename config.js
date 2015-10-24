2da8a05a82ec300cab778ced1b793950c37d1784
Melissa P <73hl10n@gmail.com>
2015-10-24 13:31:45 -0700

    Fix config, email

--- config.js
+++ config.js
@@ -1,24 +1,13 @@
 // ### Production
-  production: {
-    url: 'http://my-blog.com',
-    mail: {
-      transport: 'SMTP',
-      host: 'smtp.mandrillapp.com',
-      options: {
-        service: 'Mandrill',
-        auth: {
-          user: process.env.MANDRILL_USERNAME,
-          pass: process.env.MANDRILL_APIKEY
-        }
-      }
-    },
-    database: {
-      client: 'postgres',
-      connection: process.env.DATABASE_URL,
-      debug: false
-    },
-    server: {
-      host: '0.0.0.0',
-      port: process.env.PORT
-    }
+production: {
+  url: 'http://my-blog.com',
+  database: {
+    client: 'postgres',
+    connection: process.env.DATABASE_URL,
+    debug: false
+  },
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT
   }
+}
