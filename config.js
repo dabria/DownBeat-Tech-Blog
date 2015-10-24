7a72086807877fc716c173a72f358b307e87d0ea
Melissa P <73hl10n@gmail.com>
2015-10-24 13:38:46 -0700

    edit config

--- config.js
+++ config.js
@@ -1,13 +1,15 @@
 // ### Production
-production: {
-  url: 'http://my-blog.com',
-  database: {
-    client: 'postgres',
-    connection: process.env.DATABASE_URL,
-    debug: false
-  },
-  server: {
-    host: '0.0.0.0',
-    port: process.env.PORT
+{
+  production: {
+    url: 'http://my-blog.com',
+    database: {
+      client: 'postgres',
+      connection: process.env.DATABASE_URL,
+      debug: false
+    },
+    server: {
+      host: '0.0.0.0',
+      port: process.env.PORT
+    }
   }
 }
