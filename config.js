2acffbdffc1901cf4ad6d2a61e28210bc41cbe26
Melissa P <73hl10n@gmail.com>
2015-10-24 14:19:10 -0700

    edit config

--- config.js
+++ config.js
@@ -1,20 +1,18 @@
-config = {
-  production: {
-    url: 'http://downbeatblog.com',
-    
-    database: {
-      client: 'postgres',
-      connection: {
-        host: 'ec2-107-21-219-142.compute-1.amazonaws.com',
-        user: 'username',
-        password: 'rridscvkqkiwig',
-        database: 'd4eg5fdssfjeba',
-        port: '5432'
-      }
+// ### Production
+production: {
+  url: 'http://downbeatblog.com',
+  database: {
+    client: 'postgres',
+    connection: {
+          host: 'c2-107-21-219-142.compute-1.amazonaws.com',
+          user: 'johndown',
+          password: '43Rw2GYzLyqkrO9QYs2fPFWgd3',
+          database: 'd4eg5fdssfjeba',
+          port: '5432'
     },
-    server: {
-      host: '0.0.0.0',
-      port: process.env
-    }
+  },
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT
   }
 }
