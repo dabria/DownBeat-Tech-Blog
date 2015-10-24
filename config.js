8733a39318dd37da85828b346832e3019fff6d00
Melissa P <73hl10n@gmail.com>
2015-10-24 14:21:18 -0700

    edit config

--- config.js
+++ config.js
@@ -1,18 +1,20 @@
 // ### Production
-production: {
-  url: 'http://downbeatblog.com',
-  database: {
-    client: 'postgres',
-    connection: {
-          host: 'c2-107-21-219-142.compute-1.amazonaws.com',
-          user: 'johndown',
-          password: '43Rw2GYzLyqkrO9QYs2fPFWgd3',
-          database: 'd4eg5fdssfjeba',
-          port: '5432'
+config = {
+  production: {
+    url: 'http://downbeatblog.com',
+    database: {
+      client: 'postgres',
+      connection: {
+            host: 'c2-107-21-219-142.compute-1.amazonaws.com',
+            user: 'johndown',
+            password: '43Rw2GYzLyqkrO9QYs2fPFWgd3',
+            database: 'd4eg5fdssfjeba',
+            port: '5432'
+      },
     },
-  },
-  server: {
-    host: '0.0.0.0',
-    port: process.env.PORT
+    server: {
+      host: '0.0.0.0',
+      port: process.env.PORT
+    }
   }
 }
